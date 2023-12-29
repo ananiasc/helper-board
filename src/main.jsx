a1c260fc7eeec1a5022a4c3a7e5b73301156e783
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2023-12-28 21:47:56 -0300

    fix: continuação seguindo documentação

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import Header from './models/header/Header'
+import Footer from './models/footer/Footer'
+import Detetive from './models/boards/detetive/Detetive'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <><Header/><h1>Bem vindos</h1><Footer /></>,
+  },{
+    path: "/detetive",
+    element: <><Header/><Detetive /><Footer /></>,
+  },
+  {
+    path: "/about-us",
+    element: <><Header/>Sobre nós<Footer /></>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

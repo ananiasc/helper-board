129f26eca1c7b130ab2ab422d7c07712d1cbaac7
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2023-12-28 22:01:31 -0300

    fix: manipulação de código com limpeza de comentários

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,9 @@
 import './App.css'
-import Header from './models/header/Header'
-import Footer from './models/footer/Footer'
-import Detetive from './models/boards/detetive/Detetive'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {
-  // createBrowserRouter,
-  // RouterProvider,
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom'
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <><Header/><h1>Bem vindos</h1><Footer /></>,
-//   },{
-//     path: "/detetive",
-//     element: <><Header/><Detetive /><Footer /></>,
-//   },
-//   {
-//     path: "/about-us",
-//     element: <><Header/>Sobre nós<Footer /></>,
-//   },
-// ]);
 
 function App() {
   return (
-    // <>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" exact component={<><Header/><h1>Bem vindos</h1><Footer /></>} />
-    //     <Route path="/detetive" component={<><Header/><Detetive /><Footer /></>} />
-    //     <Route path="/about-us" component={<><Header/>Sobre nós<Footer /></>} />
-    //   </Routes>
-    // </Router>
-
-    // {/* <RouterProvider router={router} /> */}
-    // </>
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<><Header/><h1>Bem vindos</h1><Footer /></>} />
-        <Route path="/detetive" element={<><Header/><Detetive /><Footer /></>} />
-        <Route path="/about-us" element={<><Header/>Sobre nós<Footer /></>} />
-      </Routes>
-    </Router>
+    <>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
 import Header from './models/header/Header'
 import Footer from './models/footer/Footer'
@@ -30,7 +29,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
